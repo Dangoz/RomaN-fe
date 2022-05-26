0a82ba8d34e707de18dc73917cca0bfb5993e86a
Dangoz <71613713+Dangoz@users.noreply.github.com>
2022-05-25 19:56:01 -0700

    ui: Menu clean up, GradientWrapper

--- src/components/navigation/Portrait.tsx
+++ src/components/navigation/Portrait.tsx
@@ -11,14 +11,8 @@ const Portrait = ({ name, avatar }: PortraitProps) => {
   const [showMenu, setShowMenu] = useState<boolean>(false)
 
   return (
-    <div
-      className="w-[210px] h-[60px] rounded-[50px] flex justify-center items-center cursor-pointer
-      bg-gradient-to-r from-purple-400 to-pink-600"
-    >
-      <div
-        className="w-[205px] h-[55px] flex items-center bg-white rounded-[50px] transition-all duration-1000
-        hover:bg-gradient-to-r from-purple-400 to-pink-600"
-      >
+    <>
+      <GradientWrapper borderRaidus={50} width={210} height={60} borderWidth={5} hover={true}>
         <img
           className="w-[55px] h-[55px] rounded-full border-solid border-2 border-purple-500 mr-2"
           src={avatar ? avatar : '/ethShanghai.png'}
@@ -28,8 +22,8 @@ const Portrait = ({ name, avatar }: PortraitProps) => {
         <div className="text-ellipsis overflow-hidden w-[90px]">{name}</div>
 
         <ChevronDownIcon className="mx-2 h-6 w-6" />
-      </div>
-    </div>
+      </GradientWrapper>
+    </>
   )
 }
 
