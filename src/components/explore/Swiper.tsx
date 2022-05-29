2e2b87054f8bd704d590e95340eb6501f14f2917
Dangoz <71613713+Dangoz@users.noreply.github.com>
2022-05-29 13:44:38 -0700

    explore Swiper unconnected state

--- src/components/explore/Swiper.tsx
+++ src/components/explore/Swiper.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import useUser from '@/hooks/useUser'
 import useCyberConect from '@/hooks/useCyberConnect'
 import GraidentWrapper from '@/components/ui/GradientWrapper'
+import Spinner from '@/components/ui/Spinner'
 
 interface SwiperProps {
   addresses: string[]
@@ -16,10 +17,16 @@ const Swiper = ({ addresses, handleNextProfile, isLoading }: SwiperProps) => {
   const { like } = useCyberConect(provider)
 
   const handleDiscard = () => {
+    if (isLoading) {
+      return
+    }
     handleNextProfile()
   }
 
   const handleLike = () => {
+    if (isLoading) {
+      return
+    }
     like(addresses[0])
     handleNextProfile()
   }
@@ -27,36 +34,40 @@ const Swiper = ({ addresses, handleNextProfile, isLoading }: SwiperProps) => {
   return (
     <>
       <div className="w-full flex justify-center items-center mt-8">
-        <button className="w-[100px] h-[100px] rounded-[99px]" onClick={handleDiscard}>
-          <GraidentWrapper width={100} height={100} borderRaidus={99} className="flex justify-center">
-            <h1
-              className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 cursor-pointer text-2xl hover:text-white w-[100px] h-[100px] rounded-[99px] pt-9"
-              style={{ fontFamily: 'papyrus' }}
-            >
-              Discard
-            </h1>
-          </GraidentWrapper>
-        </button>
+        {address && provider && (
+          <button className="w-[100px] h-[100px] rounded-[99px]" onClick={handleDiscard}>
+            <GraidentWrapper width={100} height={100} borderRaidus={99} className="flex justify-center">
+              <h1
+                className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 cursor-pointer text-2xl hover:text-white w-[100px] h-[100px] rounded-[99px] pt-9"
+                style={{ fontFamily: 'papyrus' }}
+              >
+                Discard
+              </h1>
+            </GraidentWrapper>
+          </button>
+        )}
 
         <div className="w-[500px] h-[500px] border-2 border-purple-700 flex justify-center items-center mx-[100px]">
-          {addresses[0]}
+          {isLoading ? <Spinner /> : <div>{addresses[0]}</div>}
         </div>
 
-        <button className="w-[100px] h-[100px] rounded-[99px]" onClick={handleLike}>
-          <GraidentWrapper
-            width={100}
-            height={100}
-            borderRaidus={99}
-            className="flex justify-center hover:text-white z-20"
-          >
-            <h1
-              className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 cursor-pointer text-2xl hover:text-white w-[100px] h-[100px] rounded-[99px] pt-9"
-              style={{ fontFamily: 'papyrus' }}
+        {address && provider && (
+          <button className="w-[100px] h-[100px] rounded-[99px]" onClick={handleLike}>
+            <GraidentWrapper
+              width={100}
+              height={100}
+              borderRaidus={99}
+              className="flex justify-center hover:text-white z-20"
             >
-              Like
-            </h1>
-          </GraidentWrapper>
-        </button>
+              <h1
+                className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 cursor-pointer text-2xl hover:text-white w-[100px] h-[100px] rounded-[99px] pt-9"
+                style={{ fontFamily: 'papyrus' }}
+              >
+                Like
+              </h1>
+            </GraidentWrapper>
+          </button>
+        )}
       </div>
     </>
   )
