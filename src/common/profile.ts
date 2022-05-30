33de1e2ffb7874356d20c59f44d38f23fa8a0980
Dangoz <71613713+Dangoz@users.noreply.github.com>
2022-05-29 19:23:00 -0700

    switch back to prior implementation

--- src/common/profile.ts
+++ src/common/profile.ts
@@ -14,32 +14,24 @@ const profileGenerator = async (address: string): Promise<IProfile> => {
     infuraProjectID: config.infuraID,
   })
 
-  const profilePromise = rss3.getProfile(address, unidata)
-  const linksCountPromise = cyberconnect.getLinksCount(address)
-  const assetsPromise = rss3.getAssets(address, unidata)
-  const footprintsPromise = rss3.getFootprints(address, unidata)
-  const notesPromise = rss3.getNotes(address, 1, unidata)
-
-  const data = await Promise.all([profilePromise, linksCountPromise, assetsPromise, footprintsPromise, notesPromise])
-
   // get profile
-  const profiles = data[0]
+  const profiles = await rss3.getProfile(address, unidata)
   const uniProfile = profiles.list[0]
   const domain = uniProfile?.name || uniProfile?.username || ''
   const avatar = uniProfile?.avatars ? uniProfile.avatars[0] : ''
   const bio = uniProfile?.bio || ''
 
   // get links count
-  const { followerCount, followingCount } = data[1]
+  const { followerCount, followingCount } = await cyberconnect.getLinksCount(address)
 
   // get assets
-  const assets = data[2]
+  const assets = await rss3.getAssets(address, unidata)
 
   // get footprints
-  const footprints = data[3]
+  const footprints = await rss3.getFootprints(address, unidata)
 
   // get notes
-  const notes = data[4]
+  const notes = await rss3.getNotes(address, 1, unidata)
 
   const profile: IProfile = {
     address,
@@ -56,3 +48,62 @@ const profileGenerator = async (address: string): Promise<IProfile> => {
 }
 
 export default profileGenerator
+
+// import IProfile from '@/types/profile'
+// import cyberconnect from './cyberconnect'
+// import rss3 from './rss3'
+// import Unidata from 'unidata.js'
+// import config from './config'
+
+// /**
+//  * Given an address, generate user profile data with domain name, avatar,
+//  * links, assets/nfts, footprints/poap, notes/content.
+//  */
+// const profileGenerator = async (address: string): Promise<IProfile> => {
+//   // initialize unidata
+//   const unidata = new Unidata({
+//     infuraProjectID: config.infuraID,
+//   })
+
+//   const profilePromise = rss3.getProfile(address, unidata)
+//   const linksCountPromise = cyberconnect.getLinksCount(address)
+//   const assetsPromise = rss3.getAssets(address, unidata)
+//   const footprintsPromise = rss3.getFootprints(address, unidata)
+//   const notesPromise = rss3.getNotes(address, 1, unidata)
+
+//   const data = await Promise.all([profilePromise, linksCountPromise, assetsPromise, footprintsPromise, notesPromise])
+
+//   // get profile
+//   const profiles = data[0]
+//   const uniProfile = profiles.list[0]
+//   const domain = uniProfile?.name || uniProfile?.username || ''
+//   const avatar = uniProfile?.avatars ? uniProfile.avatars[0] : ''
+//   const bio = uniProfile?.bio || ''
+
+//   // get links count
+//   const { followerCount, followingCount } = data[1]
+
+//   // get assets
+//   const assets = data[2]
+
+//   // get footprints
+//   const footprints = data[3]
+
+//   // get notes
+//   const notes = data[4]
+
+//   const profile: IProfile = {
+//     address,
+//     domain,
+//     avatar,
+//     bio,
+//     followerCount,
+//     followingCount,
+//     assets: assets.list,
+//     footprints: footprints.list,
+//     notes: notes.list,
+//   }
+//   return profile
+// }
+
+// export default profileGenerator
